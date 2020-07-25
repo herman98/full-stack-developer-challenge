461b2e665f26e562d5c3877e9250c1a7ef439094
budi.hermansyah@fazzdoc.com <budi.hermansyah@fazzdoc.com>
2020-07-25 16:50:20 +0700

    merge with latest

---
+++ frontend/build/precache-manifest.57ad3f28efc6b7b4412cfd3e0449d4fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1de2523e03df5b0b7ce2b89fd9f0c809",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0f017ace855e25403de",
+    "url": "/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "ffb5d958cdf089cf92e3",
+    "url": "/static/css/main.c498eb9d.chunk.css"
+  },
+  {
+    "revision": "e0f017ace855e25403de",
+    "url": "/static/js/2.d53d48d2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d53d48d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffb5d958cdf089cf92e3",
+    "url": "/static/js/main.2e7c7602.chunk.js"
+  },
+  {
+    "revision": "6c281f786986ccff68b9",
+    "url": "/static/js/runtime-main.438b7e70.js"
+  }
+]);
